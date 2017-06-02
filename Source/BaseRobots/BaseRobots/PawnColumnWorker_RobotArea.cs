d9fac6533c0d6dcddce202fb757c51a54fc2cf2c
Brooks Johnson <brooks.johnson88@gmail.com>
2017-06-01 19:10:48 -0500

    Add column worker so robots can be assigned areas
    The PawnColumnWorker_AllowedArea class will only let HumanLikes be
    assigned to normal areas, so previously the robots could only be
    assigned to animal areas from the Robots table.  This allows them
    to be assigned to Human and Animal areas.

---
+++ Source/BaseRobots/BaseRobots/PawnColumnWorker_RobotArea.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+using RimWorld;
+
+namespace BaseRobot
+{
+	public class PawnColumnWorker_RobotArea : PawnColumnWorker_AllowedArea
+	{
+		//
+		// Methods
+		//
+		public override void DoCell (Rect rect, Pawn pawn, PawnTable table)
+		{
+			// Allow robots to be assigned to any area
+			AllowedAreaMode mode = AllowedAreaMode.Any;
+			AreaAllowedGUI.DoAllowedAreaSelectors (rect, pawn, mode);
+		}
+	}
+}
